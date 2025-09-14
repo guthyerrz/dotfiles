30b24153958e32f3754de50b8687301c0cfd7e53
guthyerrz <guthyerrz.ufcg@gmail.com>
2025-09-14 12:40:29 -0300

    minor refactor

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -5,24 +5,15 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
   {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#guthy-air
     darwinConfigurations."guthy-air" = nix-darwin.lib.darwinSystem {
       modules = [ 
         ./configuration.nix
-        home-manager.darwinModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.guthy = ./home.nix;
-        }
       ];
     };
   };
